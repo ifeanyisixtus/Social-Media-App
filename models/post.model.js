d36d4da0bcbcf104a2df830f4bbf9bb1141668d7
ifeanyisixtus <attahsixtus6@gmail.com>
2023-03-12 10:31:25 +0100

    done with user authentication and features

--- models/post.model.js
+++ models/post.model.js
@@ -7,7 +7,7 @@ const postSchema = new mongoose.Schema(
       ref: "User",
     },
     title: { type: String },
-    body: { type: String, required: true },
+    description: { type: String, required: true },
 
     email: { type: String, required: true, unique },
     password: { type: String, required: true, minLength: 8 },
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,11 +6,12 @@ const {
   updateUser,
   deleteUser,
 } = require("../controllers/user.controller");
+const { validateSignup } = require("../middlewares/validation.middleware");
 
 const router = Router();
 
 router.get("/", findUsers);
-router.get("/", createUser);
+router.post("/", validateSignup, createUser);
 router.get("/:id", fetchSingleUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
